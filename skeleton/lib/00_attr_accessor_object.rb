fd4671484925b3beb8d12b2308df7cc24348d486
Vera Ho <vh71886@gmail.com>
2022-05-03 16:09:53 -0400

    completed my_attr_accessor

--- skeleton/lib/00_attr_accessor_object.rb
+++ skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,16 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method("#{name}") do
+        instance_variable_get("@#{name}")
+      end
+      
+      define_method("#{name}=") do |value|
+        instance_variable_set("@#{name}", value)
+      end
+    end
   end
 end
+
+# To aid in debugging, we've also coded an option into the skeleton so you can print all the queries and interpolation arguments that get sent to the SQL engine. To enable this, pass PRINT_QUERIES=true as an ENV variable when you run the rspec command.
+# $ PRINT_QUERIES=true rspec spec/01_sql_object_spec.rb
